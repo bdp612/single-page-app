cbafe38e8c349f1356fc96112d2385099d0056b9
bdp612 <braedanpurcell@gmail.com>
2015-02-17 21:59:39 -0500

    SPA from tutorial
    A single page application that quizzes "geek knowledge," developed from
    an online tutorial.

---
+++ Geek Quiz 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Geek_Quiz_2.Startup))]
+namespace Geek_Quiz_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
